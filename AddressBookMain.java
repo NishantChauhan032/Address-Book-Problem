3e16922973cca3913ac0f819c144dde4d81c5c7f
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-04 20:06:28 +0530

    [NISHANT] add : Ability to delete a person

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -2,11 +2,24 @@ import java.util.*;
 public class AddressBookMain{
         List<Contact> addressBook = new ArrayList<Contact>();
 		static Scanner sc=new Scanner(System.in);
-		public void addContact() {
+		
+		public boolean deleteContact(String name) {
+        boolean retrn=false;
+        for(Contact c:addressBook) {
+            if(name.equals(c.getFirstName())) {
+                addressBook.remove(c);
+                retrn= true;
+            }else
+        retrn= false;
+        }
+        return retrn;
+	}	
+	
+	public void addContact() {
 			
 		Contact con=new Contact();
         char want='Y';
-        while(want=='Y'){
+        while(want == 'Y'){
             System.out.println("Enter Contact Details\nEnter First Name:");
                 con.setFirstName(sc.next());
             System.out.println("Enter Last Name:");
@@ -20,7 +33,7 @@ public class AddressBookMain{
             System.out.println("Enter Email Address:");
                 con.setEmail(sc.next());
             System.out.println("Want to add new Contact? (Y/N ):");
-            want=sc.next().charAt(0);
+            want = sc.next().charAt(0);
             addressBook.add(con);
 		}
     }
@@ -30,8 +43,8 @@ public class AddressBookMain{
             if(c.getFirstName().equals(name)) {
                 while (symbol) {
                     System.out.println("To edit,select a Number:\n 1.firstName\n 2.lastName\n 3.address\n 4.zip\n 5.phoneNo\n 6.email\n 7.exit");
-                    int select = sc.nextInt();
-                    switch (select) {
+                    int check = sc.nextInt();
+                    switch (check) {
                         case 1:
                         {System.out.println("Enter First Name:");
                             String firstname = sc.next();
@@ -88,7 +101,19 @@ public class AddressBookMain{
         System.out.println("Enter name of contact to be edited:");
         String nam=sc.next();
         adBook.editContact(nam);
-
-        }
+        
+		char entry='Y';
+		while(entry=='Y'){
+			System.out.println("Delete Contact\nEnter name of contact to be deleted");
+		    String naam = sc.next();
+			boolean del = adBook.deleteContact(naam);
+			if (del == true) System.out.println("Contact Deleted!");
+            else 
+				System.out.println("Contact not found");
+            System.out.println("Want to Delete a Contact?(Y/N)");
+            entry = sc.next().charAt(0);
+		}
+		
+    }
     
 }
